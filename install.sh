a444a8c9b02a3604c4969fb2be4d7c19d206d0d2
lukynet <98685127+LukyNet21@users.noreply.github.com>
2024-10-17 21:54:46 +0200

    adding hyprlock, notifications, idle, screenshots, volume and changing keybinds

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ ln -s ~/.dotfiles/waybar ~/.config/waybar
 ln -s ~/.dotfiles/wofi ~/.config/wofi
 ln -s ~/.dotfiles/hypr ~/.config/hypr
 ln -s ~/.dotfiles/kitty ~/.config/kitty
+ln -s ~/.dotfiles/swaync ~/.config/swaync
+
